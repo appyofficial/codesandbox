b0cde2fed7af35b9f20e29152869582dbc389ede
Appy Sharma <appy70890@gmail.com>
2020-04-13 14:01:27 +0200

    update todolist

---
+++ src/Components/TodoList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TodoList(props) {
+  return <Paper />;
+}
+
+export default TodoList;
--- src/Screen/Todo.js
+++ src/Screen/Todo.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
+import ToolBar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
 
 function Todo() {
-  return <div>This is a todo app!</div>;
+  const IniValue = [
+    "I am going to do shopping",
+    "Write letter.",
+    "Complete emails."
+  ];
+  const [Todo, setTodo] = useState(IniValue);
+  return (
+    <Paper style={{ width: "100vw", height: "100vh", padding: "20px" }}>
+      <AppBar>
+        <ToolBar>
+          <Typography>Todo List</Typography>
+        </ToolBar>
+      </AppBar>
+    </Paper>
+  );
 }
 
 export default Todo;
